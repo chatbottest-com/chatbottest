8d78ca7402946c89f9f2299c4a24a432c6527fb8
jesus.martin <jesusmartin@BEEVAs-MacBook-Air-4.local>
2017-09-28 19:02:18 +0200

    imagenes y estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,32 +52,32 @@
 		// Off-Canvas Navigation.
 
 			// Navigation Button.
-				$(
-					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+				// $(
+					// '<div id="navButton">' +
+					// 	'<a href="#navPanel" class="toggle"></a>' +
+				// 	'</div>'
+				// )
+				// 	.appendTo($body);
 
 			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							'<a href="index.html" class="link depth-0">Home</a>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'top',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
+			// 	$(
+				// 	'<div id="navPanel">' +
+				// 		'<nav>' +
+				// 			'<a href="index.html" class="link depth-0">Home</a>' +
+				// 			$('#nav').navList() +
+				// 		'</nav>' +
+				// 	'</div>'
+			// 	)
+				// 	.appendTo($body)
+				// 	.panel({
+				// // 		delay: 500,
+				// 		hideOnClick: true,
+				// 		resetScroll: true,
+				// 		resetForms: true,
+				// 		side: 'top',
+				// 		target: $body,
+				// 		visibleClass: 'navPanel-visible'
+				// 	});
 
 			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
 				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
